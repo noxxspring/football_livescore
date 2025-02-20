637589d5c043da2f8ab69fc1094715ae55961361
gabeman <gabrielayochukwu@gmail.com>
2025-02-20 16:31:38 +0100

    cors policy

--- src/main/java/com/example/Football_updates/FootballUpdateController.java
+++ src/main/java/com/example/Football_updates/FootballUpdateController.java
@@ -5,16 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@CrossOrigin("*")
 public class FootballUpdateController {
 
     @Value("${telex.integration.app_name}")
